381728070f3d927deb2079a4b889474dfc9cec27
Toli <toli@flipdish.com>
2023-08-25 08:56:13 +0100

    add posts using react query

--- src/App.js
+++ src/App.js
@@ -2,17 +2,38 @@ import React, { useState } from 'react';
 import './App.css';
 import Posts from './components/Posts';
 import PostsAxios from './components/PostsAxios';
+import PostsReactQuery from './components/PostsReactQuery';
+
 
 function App() {
-  const [showPosts, setShowPosts] = useState(true);
+  const [currentComponent, setCurrentComponent] = useState('Posts');
+
+  const handleDropdownChange = (event) => {
+    setCurrentComponent(event.target.value);
+  };
+  const renderComponent = () => {
+    switch(currentComponent) {
+      case 'Posts':
+        return <Posts />;
+      case 'PostsAxios':
+        return <PostsAxios />;
+      case 'PostsReactQuery':
+        return <PostsReactQuery />;
+      default:
+        return <Posts />;
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={() => setShowPosts(!showPosts)}>
-          Switch to different API method
-        </button>
-
-        {showPosts ? <Posts /> : <PostsAxios />}
+        <select value={currentComponent} onChange={handleDropdownChange}>
+          <option value="Posts">Posts</option>
+          <option value="PostsAxios">Posts using Axios</option>
+          <option value="PostsReact Query">Posts using React Query</option>
+        </select>
+        
+        {renderComponent()}
         
       </header>
     </div>
